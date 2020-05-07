6ef5eb88e74eeca01d478d7d91ea8458313a0ab3
Miles Danswan <miles.danswan@gmail.com>
2020-05-07 14:35:47 +1000

    Recreated Project Files due to Android Studio update issue

--- GhostTransmission/app/src/main/java/com/deakin/ghosttransmission/MainActivity.java
+++ GhostTransmission/app/src/main/java/com/deakin/ghosttransmission/MainActivity.java
@@ -1,21 +1,8 @@
 package com.deakin.ghosttransmission;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
